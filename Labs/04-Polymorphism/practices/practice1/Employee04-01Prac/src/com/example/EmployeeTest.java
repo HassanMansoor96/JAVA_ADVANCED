88f79e6844ede9db4f43a20014f161036668c1e8
Hassan Mansoor <hassan.mansoor96@gmail.com>
2022-07-26 10:42:13 +0200

    practice 4.1

--- Labs/04-Polymorphism/practices/practice1/Employee04-01Prac/src/com/example/EmployeeTest.java
+++ Labs/04-Polymorphism/practices/practice1/Employee04-01Prac/src/com/example/EmployeeTest.java
@@ -1,6 +1,6 @@
 package com.example;
 
-
+import com.example.business.EmployeeStockPlan;
 import com.example.domain.Admin;
 import com.example.domain.Director;
 import com.example.domain.Employee;
@@ -9,7 +9,7 @@ import com.example.domain.Manager;
 
 public class EmployeeTest {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
 
         // Create the classes as per the practice
         Engineer eng = new Engineer(101, "Jane Smith", "012-34-5678", 120_345.27);
@@ -20,18 +20,26 @@ public class EmployeeTest {
 
         Director dir = new Director(12, "Susan Wheeler", "099-45-2340", 120_567.36, "Global Marketing", 1_000_000.00);
 
-        eng.printEmployee();
-
-        adm.printEmployee();
-        mgr.printEmployee();
-        dir.printEmployee();
+        // Test the EmployeeStockPlan class
+        EmployeeStockPlan esp = new EmployeeStockPlan();
+        printEmployee(eng, esp);
+        printEmployee(adm, esp);
+        printEmployee(mgr, esp);
+        printEmployee(dir, esp);
 
         System.out.println("\nTesting raiseSalary and setName on Manager:");
         mgr.setName("Barbara Johnson-Smythe");
         mgr.raiseSalary(10_000.00);
-        mgr.printEmployee();
+        printEmployee(mgr, esp);
     }
 
-    
+    public static void printEmployee(Employee emp) {
+        System.out.println("Employee type: " + emp.getClass().getSimpleName());
+        System.out.println(emp);
+    }
 
+    public static void printEmployee(Employee emp, EmployeeStockPlan esp) {
+        printEmployee (emp);
+        System.out.println("Stock Options:   " + esp.grantStock(emp));
+    }
 }
